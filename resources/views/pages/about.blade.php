21de8d1040388fbe2d051d4f36db50864dbf29de
hoa44 <hoa44@njit.edu>
2019-03-26 15:26:21 -0400

    Created website template views and routes.

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.default')
+
+@section('content')
+
+
+        <h1>This will be my About Page</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+
+
+@endsection
+
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.default')
+
+@section('content')
+
+
+    <h1>This will be my Contact Page</h1>
+    <p class="lead">Please use this form to contact the site owner.</p>
+
+    <form action="/contact" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+        </div>
+
+        <div class="form-group">
+            <label for="body">Contact Message</label>
+            <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+    </form>
+
+
+@endsection
